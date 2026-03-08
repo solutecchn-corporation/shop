b9cfa6bf59fa7ea9be22d0ae648cf726e63ffb6e
Angel Guillen <angelguillen9548@gmail.com>
2026-03-07 18:02:18 -0600

    fix: publish static build for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/shop/' : '/',
+  base: command === 'build' ? '/shop/docs/' : '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 }))
